22053d7fae540836954bfbd530670e4ecbf14435
Marlon Alves <marlondev.alves@gmail.com>
2022-07-16 21:33:50 -0300

    Accordion list

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+// Função tab navigation
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
 
-  // Function to add and remove the "ativo" class
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+    // Função para adicionar e remover a classe "ativo"
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", function () {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("ativo");
   }
-
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", function () {
-      activeTab(index);
-    });
-  });
 }
+initTabNav();
+
+// Função 'accordion' para expandir e ocultar perguntas do FAQ.
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add("activeClass");
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordion();
