7014a318c1beab7c497b290b04fbc25652fcb67b
Marlon Alves <marlondev.alves@gmail.com>
2022-07-19 16:36:42 -0300

    Scroll animation

--- script.js
+++ script.js
@@ -63,4 +63,25 @@ function initScrollSuave() {
     link.addEventListener("click", scrollToSection);
   });
 }
-initScrollSuave()
+initScrollSuave();
+
+// Animação no scroll
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        console.log(sectionTop);
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    window.addEventListener("scroll", animaScroll);
+    animaScroll();
+  }
+}
+initAnimacaoScroll();
